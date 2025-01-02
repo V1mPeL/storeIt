04892d401aeaa2093b0f796c8b86ecad6423a361
Sprynskyi Vitaliy <121flame121@gmail.com>
2025-01-02 21:07:13 +0200

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '100mb',
+    },
+  },
   /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'cdn.pixabay.com' },
+      {
+        protocol: 'https',
+        hostname: 'cloud.appwrite.io',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
